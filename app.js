d1bac705393b4ff12c47bd2e78e1fe7ece437e62
tmenture <thomasoxemail@gmail.com>
2022-04-25 18:29:07 -0400

    HTML layour in its own file, generates basic portfolio skeleton

--- app.js
+++ app.js
@@ -1,40 +1,15 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
 const [name, github] = profileDataArgs;
 
-// const printProfileData = profileDataArr => {
-//   // This...
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   }
+fs.writeFile('./index.html', generatePage(name, github), err => {
 
-//   console.log('================');
+    if (err) throw err;
 
-//   // Is the same as this...
-//   profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
+    console.log('Portfolio Complete! Check out index.html to see the output!');
 
-// printProfileData(profileDataArgs);
-
-const generatePage = (userName, githubName) => {
-    return ` 
-
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title> Portfolio Demo </title>
-    </head>
-
-    <body>
-        <h1> ${name} </h1>
-        <h2><a href="https://github.com/${github}">Github</a></h2>
-    </body>
-    </html
-    
-    `;
-};
-
-console.log(name, github);
-console.log(generatePage(name, github));
+});
