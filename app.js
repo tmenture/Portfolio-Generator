4bd734bc890851482e990c72da9b606208a28680
tmenture <thomasoxemail@gmail.com>
2022-04-28 20:42:03 -0400

    html page now loads with all data from user prompts

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generatePage = require('./src/page-template.js');
+const generatePage = require('./src/page-template');
 
 const promptUser = () => {
 
@@ -161,6 +161,11 @@ promptUser()
 
     .then(portfolioData => {
 
-        console.log(portfolioData);
+      const pageHTML = generatePage(portfolioData);
 
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
     });
+ });
